8af73474a0faf4adfdd978c5a2d57e3b647b777c
Muhammad Radya Iftikhar <radyaiftikhar@gmail.com>
2024-02-12 22:13:51 +0700

    tambah bintang

---
+++ src/components/star.vue
@@ -0,0 +1,28 @@
+<!-- Star.vue -->
+<template>
+    <div class="star" :style="{ top: `${position.y}px`, left: `${position.x}px` }"></div>
+</template>
+  
+<script>
+export default {
+    name: 'app',
+    props: ['position'],
+};
+</script>
+  
+<style scoped>
+.star {
+    width: 2px;
+    height: 2px;
+    background-color: #fff;
+    position: absolute;
+    animation: fall linear infinite;
+}
+
+@keyframes fall {
+    to {
+        transform: translateY(100vh);
+    }
+}
+</style>
+  
--- src/views/home.vue
+++ src/views/home.vue
@@ -1,6 +1,7 @@
 <template>
     <div>
         <div class="container">
+            <Star v-for="(star, index) in stars" :key="index" :position="star.position" />
             <div class="title">
                 <h1 class="title-text">Random Hadist Generator</h1>
             </div>
@@ -21,13 +22,23 @@
 <script>
 import axios from 'axios';
 
+import Star from '../components/star.vue';
 
 export default {
     name: 'app',
+    components: {
+        Star
+    },
     data() {
         return {
-            hadist: {}
+            hadist: {},
+            stars: [],
+            numberOfStars: 50,
         }
+    },
+    mounted() {
+        this.initializeStars();
+        setInterval(this.updateStars, 16);
     },
     methods: {
         generateHadist() {
@@ -42,7 +53,30 @@ export default {
             } catch (r) {
                 console.log(r)
             }
-        }
+        },
+        initializeStars() {
+            for (let i = 0; i < this.numberOfStars; i++) {
+                this.stars.push({
+                    position: {
+                        x: Math.random() * window.innerWidth,
+                        y: Math.random() * window.innerHeight,
+                    },
+                });
+            }
+        },
+        resetStarPosition(star) {
+            star.position.y = 0;
+            star.position.x = Math.random() * window.innerWidth;
+        },
+        updateStars() {
+            for (const star of this.stars) {
+                star.position.y += 1;
+
+                if (star.position.y > window.innerHeight) {
+                    this.resetStarPosition(star);
+                }
+            }
+        },
     }
 }
 </script>
@@ -58,7 +92,7 @@ export default {
     bottom: 0;
     left: 0;
     right: 0;
-    background-color: #190482;
+    background-color: #03001C;
 }
 
 .title {
@@ -67,22 +101,22 @@ export default {
     height: auto;
     text-align: center;
     margin-top: 20px;
-    color: #C2D9FF;
+    color: #B6EADA;
 }
 
 .wrapper {
     /* border: 1px solid red; */
     width: 80%;
-    height: 70vh;
+    height: fit-content;
+    max-height: 70vh;
     margin-top: 30px;
-    background-color: #C2D9FF;
+    background-color: #B6EADA;
     border-radius: 20px;
     display: flex;
     align-items: center;
-    /* justify-content: space-evenly; */
     justify-content: center;
     flex-direction: column;
-    color: #190482;
+    color: #03001C;
     overflow-x: auto;
 }
 
@@ -91,6 +125,7 @@ export default {
     width: 70%;
     height: auto;
     font-weight: bolder;
+    padding-top: 60px;
 }
 
 .hadist {
@@ -106,6 +141,7 @@ export default {
     width: 70%;
     height: auto;
     font-weight: bolder;
+    padding-bottom: 60px;
 }
 
 .generate-btn {
@@ -114,13 +150,13 @@ export default {
     padding: 10px 70px;
     border-radius: 10px;
     font-weight: bolder;
-    background-color: #C2D9FF;
-    color: #190482;
+    background-color: #B6EADA;
+    color: #03001C;
     font-size: 1.2rem;
     cursor: pointer;
     position: fixed;
     bottom: 20px;
-    border: 2px solid #190482;
+    border: 2px solid #03001C;
 }
 
 .center {
